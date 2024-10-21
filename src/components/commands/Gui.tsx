8c4fdf3bc24475f41423b1671dfe6f2779196281
Marino2004 <marinomiha7@gmail.com>
2024-10-21 18:16:58 +0300

    update gui link

--- src/components/commands/Gui.tsx
+++ src/components/commands/Gui.tsx
@@ -10,7 +10,7 @@ const Gui: React.FC = () => {
 
   /* ===== check current command makes redirect ===== */
   if (rerender && currentCommand[0] === "gui") {
-    window.open("https://github.com/Marino2004", "_blank");
+    window.open("https://marino-miha.vercel.app", "_blank");
   }
 
   return <span></span>;
